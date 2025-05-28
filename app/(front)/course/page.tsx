c568d6bdb72c9dcea8b41750f97513a6c185f499
beach <meithamonwan2593@gmail.com>
2025-05-28 15:13:42 +0700

    course page (complete)

--- app/(front)/course/page.tsx
+++ app/(front)/course/page.tsx
@@ -1,10 +1,14 @@
 import Features05Page from "@/components/features-05/features-05";
 import Footer05Page from "@/components/footer-05/footer-05";
 
-export default function CoursePage() {
+export default async function Course() {
+    const data = await fetch('https://api.codingthailand.com/api/course', {cache: 'no-store'});
+    const courses = await data.json();
     return (
         <>
-            <Features05Page />
+            {
+                courses && <Features05Page courses={courses.data} />
+            }
             <Footer05Page />
         </>
     );
