f666862b070496fc17d85cf520c1f522b0b5a63d
beach <meithamonwan2593@gmail.com>
2025-05-28 10:15:58 +0700

    hero02 update (home page)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import App from "next/app";
 import AppWelcome from "./components/AppWelcome";
+import Hero02 from "@/components/hero-02/hero-02";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <hr />
-      <AppWelcome headTitle = "SWU" isShow = {true} />
-    </div>
-  )
+    <>
+      <Hero02 />
+    </>
+  );
 };
