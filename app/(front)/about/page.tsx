473849491cc3ac1fe24c938e34db900bf62763aa
beach <meithamonwan2593@gmail.com>
2025-05-28 14:13:12 +0700

    api update

--- app/(front)/about/page.tsx
+++ app/(front)/about/page.tsx
@@ -1,10 +1,15 @@
 import Contact01Page from "@/components/contact-01/contact-01";
 import Footer05Page from "@/components/footer-05/footer-05";
+import { verify } from "crypto";
 
-export default function About() {
+export default async function About() {
+    const data = await fetch('https://api.codingthailand.com/api/version');
+    const apiInfo = await data.json();
     return (
         <>
-            <Contact01Page />
+            {
+                apiInfo && <Contact01Page version={apiInfo.data.version} />
+            }
             <Footer05Page />
         </>
     );
